a43d365b773b46a17ffa44ba4c2772ac84c8acba
Weronika Sleboda <weronika_sleboda@hotmail.com>
2025-02-26 16:09:20 +0100

    Bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/v1/auth': {
-        target: 'https://phantomauth-website.onrender.com',
-        changeOrigin: true
-      }
-    }
-  },
   build: {
     minify: 'esbuild',
     sourcemap: false
