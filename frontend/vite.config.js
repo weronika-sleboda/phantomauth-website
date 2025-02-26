7616420d2a1fa19cf4914ee1f886c8ac732bb577
Weronika Sleboda <weronika_sleboda@hotmail.com>
2025-02-26 15:28:36 +0100

    Bug fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
     proxy: {
       '/api/v1/auth': {
-        target: 'http://localhost:5001',
+        target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
